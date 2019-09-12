ef6240aa60106dcf12bbae8e652185eeb9fcfe43
Thor Erik Lie <thor@thorerik.com>
2019-09-12 21:59:21 +0200

    Add checksums from scaleways own release checksums

--- automatic/scaleway-cli/update.ps1
+++ automatic/scaleway-cli/update.ps1
@@ -1,6 +1,9 @@
 import-module au
 
 $releases = 'https://github.com/scaleway/scaleway-cli/releases'
+$version = ""
+$url32 = ""
+$url64 = ""
 
 function global:au_SearchReplace {
     @{
@@ -13,20 +16,28 @@ function global:au_SearchReplace {
      }
 }
 
-function global:au_GetLatest {
+function getReleases {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-
-    #tidy-5.1.25-win64.zip
     $re  = "scw-windows-(i386|amd64).exe"
-    $url = $download_page.links | ? href -match $re | select -First 2 -expand href
+    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 2 -expand href
 
-    $version_temp = $download_page.links | ? href -match "scw_(\d*\.\d*)_amd64.deb"
-    $version = $version_temp[0] -split '_' | select -Last 1 -Skip 1
+    $checksums_url_partial = $download_page.links | Where-Object href -Match "SHA256SUMS" | Select-Object -First 1 -ExpandProperty href
+    $checksums_url = 'https://github.com' + $checksums_url_partial
+    $checksums_result = Invoke-WebRequest -Uri $checksums_url -UseBasicParsing
+    $checksums = [regex]::Matches($checksums_result.toString(), "\n(.+)\sscw-windows-(?:i386|amd64).exe\n").captures.groups
+
+    $version_temp = $download_page.links | Where-Object href -match "scw_(\d*\.\d*)_amd64.deb"
+    $version = $version_temp[0] -split '_' | Select-Object -Last 1 -Skip 1
     $url32 = 'https://github.com' + $url[0]
     $url64 = 'https://github.com' + $url[1]
+    $checksum64 = $checksums[3].toString().trim()
+    $checksum32 = $checksums[1].toString().trim()
 
-    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
-    return $Latest
+    return @{URL32 = $url32; URL64 = $url64; Version = $version; Checksum32 = $checksum32; Checksum64 = $checksum64}
 }
 
+function global:au_GetLatest {
+    $Latest = getReleases
+    return $Latest
+}
 update
