8220e3e751ba0a6e8e7db9bb1c8cb8836bfa28da
Thor Erik Lie <thor@thorerik.com>
2020-08-13 09:26:12 +0200

    Add back 32bit and make sure regexes make more or less sense

--- automatic/scaleway-cli/tools/chocolateyInstall.ps1
+++ automatic/scaleway-cli/tools/chocolateyInstall.ps1
@@ -1,8 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32 = 'https://github.com/scaleway/scaleway-cli/releases/download/v1.20/scw-windows-amd64.exe'
+$checksum32      = 'c50e3c498350a6eafdd84a241dfc2f2de00d39891de755e4c45ae514ec660df1'
+$checksumType32  = 'sha256'
 $url64 = 'https://github.com/scaleway/scaleway-cli/releases/download/v1.20/scw-windows-i386.exe'
 $checksum64    = '83aa97822ccec83d0c886d7f2894aaf9938a7703a99e716beb912a44d8a3e6cd'
 $checksumType64= 'sha256'
 
-Get-ChocolateyWebFile -PackageName 'scaleway-cli' -FileFullPath "$toolsDir\scw.exe" -Url64 $url64 -Checksum64 $checksum64 -ChecksumType64 $checksumType64
+Get-ChocolateyWebFile -PackageName 'scaleway-cli' -FileFullPath "$toolsDir\scw.exe" -Url $url32 -Url64 $url64 -Checksum $checksum32 -ChecksumType $checksumType32 -Checksum64 $checksum64 -ChecksumType64 $checksumType64
+
--- automatic/scaleway-cli/update.ps1
+++ automatic/scaleway-cli/update.ps1
@@ -5,6 +5,8 @@ $releases = 'https://github.com/scaleway/scaleway-cli/releases'
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
@@ -13,20 +15,37 @@ function global:au_SearchReplace {
 
 function getReleases {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $re  = "scw-(\d+-\d+-\d+).*-windows-x86_64.exe"
-    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+
+    $re32  = "scw-(\d+-\d+-\d+).*-windows-386.exe"
+    $url32 = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+
+    $re64  = "scw-(\d+-\d+-\d+).*-windows-x86_64.exe"
+    $url64 = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
 
     $checksums_url_partial = $download_page.links | Where-Object href -Match "SHA256SUMS" | Select-Object -First 1 -ExpandProperty href
+
     $checksums_url = 'https://github.com' + $checksums_url_partial
     $checksums_result = Invoke-WebRequest -Uri $checksums_url -UseBasicParsing
-    $checksums = [regex]::Matches($checksums_result.toString(), "\n(.+)\sscw-(\d+-\d+-\d+(?:.+))-windows-x86_64.exe\n").captures.groups
 
-    $version = $checksums[2] -split '-' -join '.'
-    
-    $url64 = 'https://github.com' + $url
-    $checksum64 = $checksums[1].toString().trim()
+    # Fetch checksums for each variant and gather version number
+    $checksum64 = [regex]::Matches($checksums_result.toString(), "(.+)\s+scw-(\d+-\d+-\d+)-windows-x86_64.exe").captures.groups
+    $checksum32 = [regex]::Matches($checksums_result.toString(), "(.+)\s+scw-(\d+-\d+-\d+)-windows-386.exe").captures.groups
 
-    return @{URL64 = $url64; Version = $version; Checksum64 = $checksum64}
+    $version = $checksum32[2] -split '-' -join '.'
+
+    $url32 = 'https://github.com' + $url32
+    $checksum32 = $checksum32[1].toString().trim()
+
+    $url64 = 'https://github.com' + $url64
+    $checksum64 = $checksum64[1].toString().trim()
+
+    return @{
+        Version = $version;
+        URL32 = $url32;
+        Checksum32 = $checksum32
+        URL64 = $url64;
+        Checksum64 = $checksum64
+    }
 }
 
 function global:au_GetLatest {
