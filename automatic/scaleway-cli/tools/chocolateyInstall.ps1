3ef4f5ed0f30f0f314962459d71478791d16883a
Chocolatey <chocolatey@thorerik.com>
2021-01-27 01:00:48 +0000

    AU: 1 updated - scaleway-cli [skip ci] https://gist.github.com/82d87d3752d48cf3b0dc029fa6fa2014

--- automatic/scaleway-cli/tools/chocolateyInstall.ps1
+++ automatic/scaleway-cli/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url32 = 'https://github.com/scaleway/scaleway-cli/releases/download/v2.2.3/scw-2.2.3-windows-386.exe'
-$checksum32      = '38ebd835223c62b117173eeaf30a15c480fdfe5489a4ceab8964556b16d17f53'
+$url32 = 'https://github.com/scaleway/scaleway-cli/releases/download/v2.2.4/scw-2.2.4-windows-386.exe'
+$checksum32      = '488edc94713606d86e410a05f8347d30ec5fd7284ee815032efd00eacbb82e29'
 $checksumType32  = 'sha256'
-$url64 = 'https://github.com/scaleway/scaleway-cli/releases/download/v2.2.3/scw-2.2.3-windows-x86_64.exe'
-$checksum64    = '62dab23b6b15107eddfb603f4f43d2787e754d5bcbc04c523b02306ef88d66c3'
+$url64 = 'https://github.com/scaleway/scaleway-cli/releases/download/v2.2.4/scw-2.2.4-windows-x86_64.exe'
+$checksum64    = '87e8649979fbd306383e84a4cc5516bd8ec0e41324daac915fb36ce3e60bc5e0'
 $checksumType64= 'sha256'
 
 Get-ChocolateyWebFile -PackageName 'scaleway-cli' -FileFullPath "$toolsDir\scw.exe" -Url $url32 -Url64 $url64 -Checksum $checksum32 -ChecksumType $checksumType32 -Checksum64 $checksum64 -ChecksumType64 $checksumType64
